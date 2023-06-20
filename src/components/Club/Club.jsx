23bb0cfc8d51341cde556e5861867505909c9d81
Vache Apinyan <apinyan.v@gmail.com>
2023-06-21 01:44:55 +0400

    feat(Club): add Club component and styles

--- src/components/Club/Club.jsx
+++ src/components/Club/Club.jsx
@@ -1,24 +1,29 @@
 import React, { Component } from "react";
+import styles from "./Club.module.css"
 
 export default class Club extends Component {
   constructor(props) {
     super(props);
 
     this.props = props;
+    this.clubData = props.clubData;
+    this.position = props.position;
+
+    const { name, image, stats } = this.clubData;
   }
+
+  
   render() {
-    return <div>Working club</div>;
+    return (
+    <div className="club">
+      ClubTitle,
+      {/* ClubInfo({this.name, this.image}),
+      ClubPoinds({ this.stats })       */}
+            </div>);
   }
 
-  componentDidMount() {
-    console.log("Club component did mount");
-  }
-
-  componentDidUpdate() {
-    console.log("Club component did update");
-  }
-
-  componentWillUnmount() {
-    console.log("Club component will unmount");
-  }
 }
+
+ClubTitle = <h4 className = {styles.clubContent}>
+{this.position ?? "-"}
+</h4> 
